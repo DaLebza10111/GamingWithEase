ab8530a6a786e7e8efdd0c4b51d59c8219892981
DaLebza10111 <lebohangsopazi3@gmail.com>
2024-10-23 11:22:19 +0200

    Add generic methods

--- GamingData/Data/DataAccess.cs
+++ GamingData/Data/DataAccess.cs
@@ -1,10 +1,30 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Protocols;
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
 
 namespace GamingData.Data
 {
     public class DataAccess
     {
         private readonly IConfiguration _config;
+
+        public DataAccess(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        public async Task<IEnumerable<T>> GetDataAsync<T, P>(string query, P parameters, string connectionId = "TransactionsDB")
+        {
+            using IDbConnection conn = new SqlConnection(_config.GetConnectionString(connectionId));
+
+            return await conn.QueryAsync<T>(query, parameters);
+        }
+
+        public async Task SaveDataAsync<P>(string query, P parameters, string connectionId = "TransactionsDB")
+        {
+            using IDbConnection conn = new SqlConnection(_config.GetConnectionString(connectionId));
+            await conn.ExecuteAsync(query, parameters);
+        }
     }
 }
