e4730be9949916e9fd88b946437c0d8c6aa066c2
DaLebza10111 <lebohangsopazi3@gmail.com>
2024-10-23 11:40:57 +0200

    Implement repository pattern

---
+++ GamingData/Repository/ClientRepository.cs
@@ -0,0 +1,32 @@
+﻿using GamingData.Models;
+
+namespace GamingData.Repository
+{
+    public class ClientRepository : IClientRepository
+    {
+        public Task<bool> AddNewClient(Client client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteClient(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Client> GetClientByID(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Client>> GetClientList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateClientDetails(Client client)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GamingData/Repository/IClientRepository.cs
@@ -0,0 +1,13 @@
+﻿using GamingData.Models;
+
+namespace GamingData.Repository
+{
+    public interface IClientRepository
+    {
+        Task<bool> AddNewClient(Client client);
+        Task<bool> UpdateClientDetails(Client client);
+        Task<Client> GetClientByID(int ID);
+        Task<bool> DeleteClient(int ID);
+        Task<IEnumerable<Client>> GetClientList();
+    }
+}
