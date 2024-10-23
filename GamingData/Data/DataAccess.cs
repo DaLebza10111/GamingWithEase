e0d12b6ffe1eba4d58e1fafb0e204e6a63247158
DaLebza10111 <lebohangsopazi3@gmail.com>
2024-10-23 11:06:49 +0200

    Install packages to Data Access layer

---
+++ GamingData/Data/DataAccess.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Protocols;
+
+namespace GamingData.Data
+{
+    public class DataAccess
+    {
+        private readonly IConfiguration _config;
+    }
+}
